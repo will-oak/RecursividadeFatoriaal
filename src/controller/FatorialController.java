5878152bf898e27fe8c36e7055a750763106fb9d
will-oak <william.pereira6@fatec.sp.gov.br>
2021-08-15 21:37:14 -0300

    Exercicio 1

--- src/controller/FatorialController.java
+++ src/controller/FatorialController.java
@@ -18,3 +18,15 @@ public class FatorialController {
 	}
 	
 }
+
+/*
+  a) A condição de parada = Quando chegar ao número 1, pois fatorial é até o número 1. 
+
+  b)Como escrever a função para o termo n em função do termo anterior 
+        
+    Coloquei para decrementar o número enquanto ele ainda não sabe o resultado e ir fazendo as multiplicações na pilha.   
+    Quando a variavel numfat chega a ponto de parada ele desfaz a pilha e soma todos os resultados, nos trazendo assim o fatorial. 
+       
+   
+    
+  */
